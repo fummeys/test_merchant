38c5ad205393dea0fdd66ec654104416201b65b4
D-hacktivator <fummeys@gmail.com>
2022-05-16 16:21:49 +0100

    setinterval and setimeout

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-class Pet {
-    taxonomy = 'animal'
-    sleep(q, name){
-        for(let i = 0;i < q; i++){
-            console.log(`${name} is sleeping for ${q} hour`)
-        }
-    }
+setTimeout(()=>{
+    // let myform = document.getElementsByClassName('form')
+    let myform = document.getElementById('form')
+    myform.style.backgroundColor = 'blue'
+    // console.log(myform)
+},3000)
+
+const reload = ()=>{
+    // window.location.href = '/'
+    console.log('working')
 }
 
-class Dog extends Pet {
-    group = 'mamal'
-    sound(q){
-        for(let i = 0;i < q; i++){
-            console.log(`${i +1} woof!`)
-        }
-    }
-}
-class Cat extends Pet{
-    group = 'mamal'
-    sound(q){
-        for(let i = 0;i < q; i++){
-            console.log(`${i +1} meoooow!`)
-        }
-    }
-}
-
-
-let bingo = new Dog()
-let susu = new Cat()
-// bingo.sound(2)
-// susu.sound(1)
-susu.sleep(2,'susu')
-
-console.log(bingo,susu)
+setInterval(reload, 10000)
